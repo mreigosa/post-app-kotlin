1da03bfde8a724b162f4f6fe150693d702bf5254
Reigosa, Martín <martin.reigosa@mobgen.com>
2020-04-19 10:06:31 +0200

    added first UI test

--- app/src/testShared/instrumentation/remotedatasource/PostRemoteDataSourceConfiguration.kt
+++ app/src/testShared/instrumentation/remotedatasource/PostRemoteDataSourceConfiguration.kt
@@ -1,8 +1,17 @@
 package instrumentation.remotedatasource
 
 import com.mreigar.data.model.PostEntity
-import instrumentation.data.PostRepositoryInstrument.givenPostEntity
+import java.util.*
 
 data class PostRemoteDataSourceConfiguration(
-    var postEntityList: List<PostEntity> = listOf(givenPostEntity())
+    var postEntityList: List<PostEntity> = givenPostEntityList(1)
 )
+
+fun givenPostEntityList(size: Int): List<PostEntity> {
+    val list: MutableList<PostEntity> = LinkedList<PostEntity>()
+    for (i in 0 until size) {
+        list.add(PostEntity(i, i, "Title - $i", "Body - $i"))
+    }
+
+    return list
+}
