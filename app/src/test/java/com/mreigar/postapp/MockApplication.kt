80a4dfd999981f3444f4512ab720329502844b86
Reigosa, Martín <martin.reigosa@mobgen.com>
2020-04-21 18:49:23 +0200

    added annotation to fix robolectric

--- app/src/test/java/com/mreigar/postapp/MockApplication.kt
+++ app/src/test/java/com/mreigar/postapp/MockApplication.kt
@@ -1,6 +1,7 @@
 package com.mreigar.postapp
 
 import android.app.Application
+import android.os.Build
 import org.junit.After
 import org.junit.Before
 import org.junit.runner.RunWith
@@ -11,7 +12,7 @@ import org.robolectric.annotation.Config
 class MockApplication : Application()
 
 @RunWith(RobolectricTestRunner::class)
-@Config(application = MockApplication::class)
+@Config(application = MockApplication::class, sdk = [Build.VERSION_CODES.P])
 abstract class AppRobolectricTestRunner : AutoCloseKoinTest() {
     @Before
     open fun before() {
